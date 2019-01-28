963b50742a3dceef35894c46ff5dc38e49f69d2b
JYK <714017560@qq.com>
2019-01-29 00:27:46 +0800

    openning场景添加

---
+++ Assets/Script/Arrow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Arrow : MonoBehaviour {
+
+    // Use this for initialization
+    int i = 0;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 v = new Vector3(-1.6f, -1.45f,0);
+            transform.position = v;
+            i = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 v = new Vector3(-1.6f, -2.3f, 0);
+            transform.position = v;
+            i = 1;
+        }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            if(i==0)
+            {
+                SceneManager.LoadScene("play");
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
